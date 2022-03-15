4fa44422e19d63ebeb70feb8cf5857c8df5946cd
Bright Amegbor <bright.amegbor@cognatesystems.com>
2022-03-15 00:48:05 +0000

    added styles

---
+++ lib/common/ejara_styles.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class EjaraStyles {
+  static const colorBackground = Color(0xfff8f8fa);
+  static const colorCardShadow = Color(0xfff9fafa);
+  static const colorBlue = Color(0xff757be4);
+  static const colorDarkerBlue = Color(0xff10155d);
+  static const colorDarkBlue = Color(0xff232668);
+  static const colorLightBlue = Color(0xff75789d);
+  static const colorWhite = Colors.white;
+}
