83281c9bdb508edf09910b9dfb3fb511e1d839f3
Bright Amegbor <bright.amegbor@cognatesystems.com>
2022-03-16 00:03:45 +0000

    added payment methods data model

--- lib/models/payment_methods.dart
+++ lib/models/payment_methods.dart
@@ -1,12 +1,42 @@
 import 'package:flutter/widgets.dart';
 
 class PaymentMethodsModel extends ChangeNotifier {
-  List _paymentMethods = [];
+  List<PaymentType> _paymentMethods = [];
 
-  List get paymentMethods => _paymentMethods;
+  List<PaymentType> get paymentMethods => _paymentMethods;
 
-  set paymentMethods(List value) {
+  set paymentMethods(List<PaymentType> value) {
     _paymentMethods = value;
     notifyListeners();
+  }
+}
+
+class PaymentType {
+  late int id;
+  late String code;
+  late String titleEn;
+  late String titleFr;
+  late String descriptionEn;
+  late String descriptionFr;
+  late String iconImage;
+
+  PaymentType({
+    required this.id,
+    required this.code,
+    required this.titleEn,
+    required this.titleFr,
+    required this.descriptionEn,
+    required this.descriptionFr,
+    required this.iconImage,
+  });
+
+  PaymentType.fromJson(Map json) {
+    id = json["id"];
+    code = json["code"];
+    titleEn = json["title_en"];
+    titleFr = json["title_fr"];
+    descriptionEn = json["description_en"];
+    descriptionFr = json["description_fr"];
+    iconImage = json["icon_image"];
   }
 }
