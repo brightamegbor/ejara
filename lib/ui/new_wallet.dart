1ccfe1e32d4535dfb3055467c47956d11e89aa0f
Bright Amegbor <bright.amegbor@cognatesystems.com>
2022-03-17 22:34:55 +0000

    finished up with new wallet screen

--- lib/ui/new_wallet.dart
+++ lib/ui/new_wallet.dart
@@ -1,41 +1,140 @@
 import 'package:ejara/common/ejara_styles.dart';
 import 'package:ejara/models/payment_methods.dart';
+import 'package:ejara/widgets/continue_button.dart';
+import 'package:ejara/widgets/dropdown_field.dart';
+import 'package:ejara/widgets/text_field.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_switch/flutter_switch.dart';
 import 'package:provider/provider.dart';
 
-class NewWallet extends StatelessWidget {
+class NewWallet extends StatefulWidget {
   final String paymentMethod;
+
   const NewWallet({Key? key, required this.paymentMethod}) : super(key: key);
+
+  @override
+  State<NewWallet> createState() => _NewWalletState();
+}
+
+class _NewWalletState extends State<NewWallet> {
+  String operatorSelected = "";
+
+  TextEditingController phoneController = TextEditingController();
+  TextEditingController nameController = TextEditingController();
+
+  bool saveAs = true;
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Consumer<PaymentMethodsModel>(
         builder: (context, model, child) => Scaffold(
-          backgroundColor: EjaraStyles.colorBackground,
+          backgroundColor: EjaraStyles.colorWhite,
           body: Container(
             padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                closeButton(context),
+            child: SingleChildScrollView(
+              child: SizedBox(
+                height: MediaQuery.of(context).size.height * 0.9,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      children: [
+                        // close button
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: closeButton(context),
+                        ),
 
-                // choose payment methods
-                Center(
-                  child: Container(
-                    padding: const EdgeInsets.only(top: 25.0, bottom: 15.0),
-                    child: Text(
-                      "New " + paymentMethod,
-                      textAlign: TextAlign.center,
-                      style: const TextStyle(
-                        color: EjaraStyles.colorDarkerBlue,
-                        fontSize: 35.0,
-                        fontWeight: FontWeight.w700,
-                      ),
+                        // new mobile
+                        Center(
+                          child: Container(
+                            padding:
+                                const EdgeInsets.only(top: 25.0, bottom: 15.0),
+                            child: Text(
+                              "New " + widget.paymentMethod,
+                              textAlign: TextAlign.center,
+                              style: const TextStyle(
+                                color: EjaraStyles.colorDarkerBlue,
+                                fontSize: 35.0,
+                                fontWeight: FontWeight.w700,
+                              ),
+                            ),
+                          ),
+                        ),
+
+                        const SizedBox(height: 20),
+
+                        // dropdownButton
+                        EjaraDropdownField(
+                          label: "Choose the mobile operator",
+                          items: const ["Orange Money", "MTN Mobile Money"],
+                          value: operatorSelected,
+                          onChanged: onChanged,
+                        ),
+
+                        const SizedBox(height: 25),
+
+                        // phone text field
+                        EjaraTextField(
+                          label: "Phone number",
+                          controller: phoneController,
+                        ),
+
+                        const SizedBox(height: 25),
+
+                        // name text field
+                        EjaraTextField(
+                          label: "Full name",
+                          controller: nameController,
+                        ),
+
+                        const SizedBox(height: 25),
+
+                        // save paymentMethod
+                        Row(
+                          children: [
+                            // switch
+                            FlutterSwitch(
+                              width: 28.0,
+                              height: 18.0,
+                              valueFontSize: 10.0,
+                              toggleSize: 10.0,
+                              value: saveAs,
+                              activeColor: EjaraStyles.colorBlue,
+                              onToggle: (val) {
+                                setState(() {
+                                  saveAs = val;
+                                });
+                              },
+                            ),
+
+                            const SizedBox(
+                              width: 8,
+                            ),
+                            Text(
+                              "Save as payment method",
+                              style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                color:
+                                    EjaraStyles.colorDarkBlue.withOpacity(0.5),
+                                fontSize: 18.0,
+                              ),
+                            )
+                          ],
+                        ),
+                      ],
                     ),
-                  ),
+
+                    // continue button
+                    Container(
+                        padding: const EdgeInsets.only(
+                          top: 30.0,
+                        ),
+                        child: const EjaraContinueButton()),
+                  ],
                 ),
-              ],
+              ),
             ),
           ),
         ),
@@ -57,4 +156,10 @@ class NewWallet extends StatelessWidget {
           ),
         ),
       );
+
+  void onChanged(String? val) {
+    setState(() {
+      operatorSelected = val!;
+    });
+  }
 }
